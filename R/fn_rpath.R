0f674f3cabd3c72c0e3664f09683d3306a5540b9
delsa <t.j.delsantooneill@qmul.ac.uk>
2021-02-28 12:59:17 +0000

    Package functions. Need to work on descriptions.

---
+++ R/fn_rpath.R
@@ -0,0 +1,162 @@
+#' Computes long-term equilibrium yields for \code{Rpath} models
+#'
+#' Description
+#'
+#' @param par
+#' @param simul.years
+#' @param aged.str
+#' @param data.years
+#' @param IDnames
+#' @param rsim.mod
+#' @param rpath.params
+#' @param avg.window
+#' @param integration.method
+#'
+#' @return
+#' @export
+fn_rpath <- function(par, simul.years = 100, aged.str, data.years,
+                     IDnames, rsim.mod, rpath.params, avg.window = 10,
+                     integration.method = "RK4") {
+  # VALIDATOR
+  if (!is.vector(par)) {
+    stop("`par` is not a vector.")
+  }
+  if (length(par) < 1) {
+    stop("`par` must be a vector of length >= 1")
+  }
+  if (!is.logical(aged.str)) {
+    stop("`aged.str` must be a logical TRUE/FALSE argument.")
+  }
+  if (length(data.years) > 1) {
+    stop("`data.years` must be a single numeric value.")
+  }
+  if (aged.str == TRUE && !is.list(rpath.params)) {
+    stop("For aged structure models, the Rpath's balanced parameter object needs
+         to be provided.")
+  }
+  if (aged.str == FALSE && length(par) < length(IDnames)) {
+    stop("The length of `par` and the length of `IDnames` must be equal for
+         non age-structured models.")
+  }
+  if (!is.list(rsim.mod)) {
+    stop("`rsim.mod` must be an rsim object created via Rpath's
+         `rsim.scenario()` function.")
+  }
+  if (simul.years != 100 & nrow(rsim.mod$fishing$ForcedFRate) != simul.years) {
+    stop("If a longer simulation is desired, you will need to create a new
+         `Rsim` scenario where the argument `years` is set to the required
+         simulation time. Then, pass `par` with the new `Rsim.model` setting
+         `simul.years` equal to the number of years within `rsim.scenario()`
+         function.")
+  }
+  if (length(avg.window) > 1) {
+    stop("`avg.window` must be a single numeric value.")
+  }
+  if (!integration.method%in%list("RK4", "AB")){
+    stop("Sorry but the integration method introduced is not implemented")
+  }
+  # LOCAL VARIABLES
+  sppname <- IDnames
+  harvesting <- par
+  simul.years <- simul.years
+  # ADJUST SCENARIO
+  if (integration.method == "AB") {
+    # Setting integration flags
+    NoIntegrateSpp <- as.vector(rsim.mod$params$spname[rsim.mod$params$PBopt > 24])
+    for (i in 1:length(NoIntegrateSpp)) {
+      name <- NoIntegrateSpp[i]
+      rsim.mod <- adjust.scenario(rsim.mod, parameter = "NoIntegrate",
+                                  group = name, value = 0)
+    }
+  }
+  # NOTE: Ensure effort is set to 0 to play with harvesting rates.
+  fleet_name <- as.character(colnames(rsim.mod$fishing$ForcedEffort))
+  for (i in 1:length(fleet_name)) {
+    rsim.mod <- adjust.fishing(rsim.mod, parameter = "ForcedEffort",
+                               group = fleet_name[i],
+                               sim.year = 1:simul.years, sim.month = 0,
+                               value = 0)
+  }
+  # AGE-STRUCTURED MODELS
+  if (aged.str == TRUE) {
+    n.aged.str <- rsim.mod$stanzas$Nsplit
+    stanza.names <- rpath.params$stanzas$stindiv$Group
+    juvname <- stanza.names[seq(1, n.aged.str*2, 2)]
+    adname <- stanza.names[seq(2, n.aged.str*2, 2)]
+    JuvFProp <- as.numeric((rsim.mod$fishing$ForcedFRate[data.years,juvname]/
+                              rsim.mod$fishing$ForcedFRate[data.years,adname]))
+    if (all.equal(IDnames[IDnames%in%stanza.names], stanza.names) == FALSE) {
+      stop("`IDnames` levels for stanza groups must be labeled as in the model
+           parameterisation.")
+    }
+    for (i in 1:length(juvname)) {
+      rsim.mod <- adjust.fishing(Rsim.scenario = rsim.mod,
+                                 parameter = "ForcedFRate",
+                                 group = juvname[i],
+                                 sim.year = (data.years+1):simul.years,
+                                 sim.month = 0,
+                                 value = harvesting[i]*JuvFProp[i])
+      rsim.mod <- adjust.fishing(Rsim.scenario = rsim.mod,
+                                 parameter = "ForcedFRate",
+                                 group = adname[i],
+                                 sim.year = (data.years+1):simul.years,
+                                 sim.month = 0, value = harvesting[i])
+    }
+    if ((length(IDnames) > length(stanza.names)) == TRUE) {
+      non.aged.groups <- IDnames[!IDnames%in%stanza.names]
+      elements <- n.aged.str + (1:length(non.aged.groups))
+      for (i in 1:length(elements)) {
+        element <- elements[i]
+        rsim.mod <- adjust.fishing(Rsim.scenario = rsim.mod,
+                                   parameter = "ForcedFRate",
+                                   group = non.aged.groups[i],
+                                   sim.year = (data.years+1):simul.years,
+                                   sim.month = 0, value = harvesting[element])
+      }
+      # RUN SIMULATION & COMPUTE YIELDS
+      rsim.simul <- rsim.run(rsim.mod, method = integration.method,
+                             years = 1:simul.years)
+      yields <- array(dim = c(nrow(rsim.simul$annual_Biomass),
+                              length(harvesting)))
+      for (i in 1:nrow(rsim.simul$annual_Biomass)) {
+        adY <- rsim.simul$annual_Biomass[i, adname] * harvesting[1:n.aged.str]
+        juvY <- rsim.simul$annual_Biomass[i, juvname] * harvesting[1:n.aged.str] * JuvFProp
+        yield <- adY + juvY
+        non.aged.yield <- rsim.simul$annual_Biomass[i, non.aged.groups] * harvesting[-c(1:n.aged.str)]
+        yields[i,] <- append(yield, non.aged.yield)
+      }
+    } else if ((length(IDnames) > length(stanza.names)) == FALSE) {
+      # RUN SIMULATION & COMPUTE YIELDS
+      rsim.simul <- rsim.run(rsim.mod, method = integration.method,
+                             years = 1:simul.years)
+      yields <- array(dim = c(nrow(rsim.simul$annual_Biomass),
+                              length(harvesting)))
+      for (i in 1:nrow(rsim.simul$annual_Biomass)) {
+        adY <- rsim.simul$annual_Biomass[i, adname] * harvesting[1:n.aged.str]
+        juvY <- rsim.simul$annual_Biomass[i, juvname] * harvesting[1:n.aged.str] * JuvFProp
+        yields[i,] <- adY + juvY
+      }
+    }
+  } else if (aged.str == FALSE) {
+    for (i in 1:length(sppname)) {
+      rsim.mod <- adjust.fishing(Rsim.scenario = rsim.mod,
+                                 parameter = "ForcedFRate",
+                                 group = sppname[i],
+                                 sim.year = (data.years+1):simul.years,
+                                 sim.month = 0, value = harvesting[i])
+    }
+    # RUN SIMULATION & COMPUTE YIELDS
+    rsim.simul <- rsim.run(rsim.mod, method = integration.method,
+                           years = 1:simul.years)
+    yields <- array(dim = c(nrow(rsim.simul$annual_Biomass), length(adname)))
+    for (i in 1:nrow(rsim.simul$annual_Biomass)) {
+      yields[i,] <- rsim.simul$annual_Biomass[i,sppname] * harvesting
+    }
+  }
+  names <- c()
+  for (i in 1:ncol(yields)) {
+    names <- append(names, paste("Spp",i))
+  }
+  colnames(yields) <- names
+  return(colMeans(tail(yields, n = avg.window)))
+}
---
+++ R/nash.R
@@ -0,0 +1,146 @@
+#' Computes Nash Equilibrium harvesting rates
+#'
+#' Description
+#'
+#' @param par
+#' @param ...
+#' @param fn
+#' @param method
+#' @param F.increase
+#' @param yield.cruves
+#' @param conv.criterion
+#'
+#' @return
+#' @export
+nash <- function(par, fn, ..., method = "LV", F.increase = 0.1,
+                 yield.curves = TRUE, conv.criterion = 0.001){
+  # VALIDATOR
+  if (!is.vector(par)) {
+    stop("`par` is not a vector.")
+  }
+  if (length(par) < 1) {
+    stop("`par` must be a vector of length >= 1")
+  }
+  if (!method%in%list("LV", "dummy")){
+    stop("Sorry but this method is not implemented")
+  }
+  if (method == "LV") {
+    # LOCAL VARIABLES
+    nSpp <- length(par)
+    nash_fncalls <- 0
+    n.iter <- 100
+    Nash_Fs <- array(dim = c(n.iter, nSpp))
+    Nash_Bs <- array(dim = c(n.iter, nSpp))
+    Nash_Rs <- array(dim = c(n.iter, nSpp))
+    yields <- fn(par, ...)
+    B.eq <- as.numeric(yields) / par
+    F.eq <- par
+    M <- matrix(nrow = nSpp, ncol = nSpp)
+    # ALGORITHM
+    # `...` is for arguments that our algorithm does not recognise but `fn` does
+    for (iter in 1:n.iter) {
+      for (i in 1:nSpp) {
+        #Parameters
+        par.p <- par
+        par.m <- par
+        par.p[i] <- par.p[i] + F.increase
+        par.m[i] <- par.m[i] - F.increase
+        #Running model
+        yields.p <- fn(par.p, ...)
+        B.p <- as.numeric(yields.p) / par.p
+        nash_fncalls <- nash_fncalls + 1
+        yields.m <- fn(par.m, ...)
+        B.m <- as.numeric(yields.m) / par.m
+        nash_fncalls <- nash_fncalls + 1
+        #Populating M
+        M[,i] <- (B.p - B.m) / (F.increase * 2)
+      }
+      if (det(M)==0){
+        extinct.name   <- names(which(colSums(M) == 0))
+        print("Initial M is singular and hence non invertible")
+        print(extinct.name)
+      }
+      G <- -1 * solve(M)
+      r <- (G %*% B.eq) + F.eq
+      G_hat <- diag(1 / (diag(solve(G))))
+      B_new <- solve(G + G_hat, r)
+      F_new <- G_hat %*% B_new
+      ##	Saving
+      Nash_Fs[iter,] <- F_new
+      Nash_Bs[iter,] <- B_new
+      Nash_Rs[iter,] <- r
+      # Re-running the model to equilibrium with new Nash Fs
+      par <- as.numeric(F_new)
+      print(max(abs(F.eq / Nash_Fs[(iter),] -1)))
+      yields <- fn(par, ...)
+      B.eq <- as.numeric(yields) / par
+      F.eq <- par
+      nash_fncalls <- nash_fncalls + 1
+      # Convergence statement
+      if (iter>1) {
+        if (max(abs(F_new / Nash_Fs[(iter-1),] -1)) < conv.criterion) {
+          print(paste("Nash equilibrium found after ", iter, " iterations with",
+                      nash_fncalls, " function calls."))
+          break
+        }
+      }
+    }
+    # OUTPUT
+    outlist <- list(Nash_Fs = Nash_Fs[1:iter,], Nash_Bs = Nash_Bs[1:iter,],
+                    Nash_Rs = Nash_Rs[1:iter,], func.evals = nash_fncalls)
+  }
+  if (method == "dummy") {
+    # LOCAL VARIABLES
+    nSpp <- length(par)
+    nash_fncalls <- 0
+    n.iter <- 100
+    Nash_Hs <- array(dim = c(n.iter, nSpp))
+    F.eq <- par
+    # Compute Spp yields one at a time
+    Yield <- function(par, Hvec, j){
+      Hvec[j] <- par
+      nash_fncalls <- nash_fncalls + 1
+      as.numeric(fn(Hvec, ...))[j]
+    }
+    for (iter in 1:n.iter) {
+      for (j in 1:nSpp) {#Length of Spp vector
+        output <- optim(par = par[j], fn = Yield, Hvec = par, j = j,
+                        control = list(fnscale = -1, abstol = conv.criterion, factr = 1e12),
+                        method = "BFGS", hessian = TRUE)
+        par[j] = output$par
+        nash_fncalls <- nash_fncalls + output$counts[1]
+      }
+      Nash_Hs[iter,] <- par
+      print(max(abs(F.eq / Nash_Hs[(iter),] -1)))
+      F.eq <- par
+      if (iter>1) {
+        if (max(abs(Nash_Hs[iter,] / Nash_Hs[(iter-1),] -1)) < conv.criterion) {
+          print(paste("Nash equilibrium found after ", iter, " iterations with",
+                      nash_fncalls, " function calls."))
+          break
+        }
+      }
+    }
+    # OUTPUT
+    outlist <- list(Nash_Fs = Nash_Hs[1:iter,], func.evals = nash_fncalls)
+  }
+  if (yield.curves == TRUE) {
+    Yield <- function(par, Hvec, j){
+      Hvec[j] <- par
+      nash_fncalls <- nash_fncalls + 1
+      as.numeric(fn(Hvec, ...))[j]
+    }
+    Fvec <- seq(0,1.5,0.025)
+    Yieldeq <- array(dim = c(length(Fvec), length(par)))
+    par <- c()
+    if (method=="dummy" | method=="LV") {
+      par <- as.numeric(tail(outlist$Nash_Fs, n = 1))
+    }
+    for (i in 1:length(par)) {
+      Yieldeq[,i] <- sapply(X = Fvec, FUN = Yield, Hvec = par, j = i)
+    }
+    outlist$Yieldeq <- Yieldeq
+  }
+  # Return
+  return(outlist)
+}
