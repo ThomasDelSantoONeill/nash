b8d80a8258d638f222e3c77ae1737a4c4bd09b4f
delsa <t.j.delsantooneill@qmul.ac.uk>
2022-10-28 08:54:07 +0100

    Nothing to commit

--- R/nash.R
+++ R/nash.R
@@ -303,7 +303,7 @@ nash <- function(par, fn, ..., method = "LV", yield.curves = FALSE,
   #   outlist <- list(par = SS_Hs[1,],
   #                   value = SS_Hs[2,])
   # }
-  ### EQUILIBRIUM YIELD CURVES
+  ### EQUILIBRIUM YIELD CURVES (test)
   if (yield.curves == TRUE) {
     Yield <- function(par, Hvec, j){
       Hvec[j] <- par
