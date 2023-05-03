a9871e6349fd33b9f2ca502d9516dd123dc8a240
delsa <t.j.delsantooneill@qmul.ac.uk>
2023-05-03 14:48:28 +0100

    Let's try not adding the yields of stanza groups

--- R/fn_rpath.R
+++ R/fn_rpath.R
@@ -157,7 +157,7 @@ fn_rpath <- function(par, simul.years = 100, aged.str = TRUE, data.years,
       for (i in 1:nrow(rsim.simul$annual_Catch)) {
         adY <- rsim.simul$annual_Catch[i, adname]
         juvY <- rsim.simul$annual_Catch[i, juvname]
-        yield <- adY + juvY
+        yield <- adY # + juvY
         non.aged.yield <- rsim.simul$annual_Catch[i, non.aged.groups]
         yields[i,] <- append(yield, non.aged.yield)
       }
@@ -170,7 +170,7 @@ fn_rpath <- function(par, simul.years = 100, aged.str = TRUE, data.years,
       for (i in 1:nrow(rsim.simul$annual_Catch)) {
         adY <- rsim.simul$annual_Catch[i, adname]
         juvY <- rsim.simul$annual_Catch[i, juvname]
-        yields[i,] <- adY + juvY
+        yields[i,] <- adY # + juvY
       }
     }
   } else if (aged.str == FALSE) {
