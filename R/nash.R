06d919786d50073960c267d5c3f03140abd565d0
delsa <t.j.delsantooneill@qmul.ac.uk>
2023-05-18 14:41:24 +0100

    Extract -G^-1 for stepsize experiment

--- R/nash.R
+++ R/nash.R
@@ -203,7 +203,7 @@ nash <- function(par, fn, ..., method = "LV", yield.curves = FALSE,
     outlist <- list(par = tail(na.omit(Nash_Fs), n = 1),
                     value = fn(as.numeric(tail(na.omit(Nash_Fs),
                                                n = 1)), ...),
-                    counts = nash_fncalls,
+                    counts = nash_fncalls, M = M,
                     convergence = paste("Nash equilibrium found after ", iter,
                                         " iterations."))
   }
